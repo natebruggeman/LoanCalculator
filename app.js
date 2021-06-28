164d33c8a1e60dcd14991f4619842a4ef7697a62
Nate Bruggeman <njbruggeman@gmail.com>
2021-06-28 11:08:26 -0500

    error message rendering on card

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 
 
 function calculateResults(e) {
-    console.log('testooo');
 
     //grabbing UI variables
     const amount = document.getElementById('amount');
@@ -25,9 +24,30 @@ function calculateResults(e) {
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
     } else {
-        console.log('error occurred nb')
+        showError('Please check your numbers')
     }
 
 
     e.preventDefault();
+}
+
+
+function showError(error) {
+    //create error div
+    const errorDiv = document.createElement('div');
+
+    //get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading')
+
+
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    //placing error message at top of card
+    card.insertBefore(errorDiv, heading);
+
+    window.setTimeout()
+
 }
