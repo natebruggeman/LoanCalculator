9540f1dbd82859d0131816c96c27a8579e964dbb
Nate Bruggeman <njbruggeman@gmail.com>
2021-06-28 10:40:17 -0500

    grabbing UI variables on app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+
+function calculateResults(e) {
+    console.log('testooo');
+
+    //grabbing UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+
+
+    e.preventDefault();
+}
